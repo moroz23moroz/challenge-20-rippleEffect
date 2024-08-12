6964411dc4d42803422def7e524ac4cd460c2a24
Marina Moroz <moroz2387moroz@yandex.ru>
2024-08-12 23:25:23 +1000

    add script

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
-
+window.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".bts");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const x = e.clientX;
+      const y = e.clientY;
+      const buttonTop = e.target.offsetTop;
+      const buttonLeft = e.target.offsetLeft;
+      const xInside = x - buttonLeft;
+      const yInside = y - buttonTop;
+      const circle = document.createElement("span");
+      circle.classList.add("circle");
+      circle.style.top = yInside + "px";
+      circle.style.left = xInside + "px";
+      this.appendChild(circle);
+      setTimeout(() => circle.remove(), 500);
+    });
+  });
+});
